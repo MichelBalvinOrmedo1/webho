ec3dcfa5319747e59a7ddcedfeedd9bcb545d1c8
Michel Balvin Ormedo <michelbalvinor@gmail.com>
2024-03-09 21:03:13 -0500

    ggg22

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -32,22 +32,10 @@ export class WebhooksController {
     } else {
       throw new Error('Invalid subscription parameters');
     }
-  } /*
+  }
   @Post()
   @HttpCode(200) // Facebook espera una respuesta con código 200
   handleWebhook(@Body() body: any) {
     return this.webhooksService.handleWebhook(body);
-  }*/
-  @Post('/webhooks') // Define que esta función manejará las solicitudes POST
-  async handleWebhook2(@Body() body: any) {
-    // Llama al servicio para manejar la lógica de la solicitud de webhook
-    return await this.webhooksService.handleWebhook2(body);
-  }
-  @Post()
-  handleWebhook(@Body() body: any) {
-    console.log('Received webhook:');
-    console.dir(body, { depth: null });
-    // Aquí puedes agregar la lógica para manejar la solicitud del webhook
-    return { message: 'Webhook received successfully' };
   }
 }
